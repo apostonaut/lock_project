2a78a24d3fad0d212bc9367405c43bd069474281
apostonaut <apostonaut@gmail.com>
2017-11-28 12:26:22 -0500

    Ray's changes to code checker Nov 28

--- code_checker.v
+++ code_checker.v
@@ -1,54 +1,87 @@
 /*The purpose of this module is to receive input signals from the keypad, store input
 values in 
 */
-module code_checker(/*pw_is_correct (outpu)t*/test, input_value, compare, bits, pw_length, resetn);//, pw_sys);
-
-	input input_value;
-	input compare;
-	input [1:0] bits;
-	input resetn;
-	//output pw_is_correct;
+module code_checker(clk, correct_password, input_value, store_value, incorrect_password, compare, bits, resetn);
+	input clk;
+	input input_value; // coming from the Controller, tellign the program to store the data values in input
+	input store_value; // coming from the Controller, tellign the program to store the data values in password
+	input compare; // coming from Controller, telling the program to compare password and input
+	input [1:0] bits; // comng from the switches
+	input resetn; // should reset the entire program
+	
 	//output done_compare
 	
 	/*IS THIS VALID?? being sent from setup_panel 
 	so can be compared to user-inputted password */
-	//input [1:0] pw_sys [3:0]; 
-	input pw_length;
-	output [1:0]test;
+	output reg correct_password; // goes to controller
+	output reg incorrect_password;
 	
 	//register to keep track of the number of characters the user has inputted
 	integer num_inputs;
+	integer num_password;
+	integer index;
 	
 	//reg [seast_significant_index:most_significant_index] array_name [num_elements:0]
 	//register array with 8 2-bit registers
-	reg [1:0] pw_in [3:0];
+	reg [1:0] pw_in [3:0]; // input 
+	reg [1:0] pw_sys [3:0]; // actual password
 	
+	integer counter;
 	
-	always @(posedge input_value) begin
+	always @(~compare) begin //maybe use clock???
+		
 		//MUST begin by resetting system in order for this to work!!
 		if (!resetn) begin
 			num_inputs <= 0;
-			pw_in[0] <= 2'd0;
-			pw_in[1] <= 2'd0;
-			pw_in[2] <= 2'd0;
-			pw_in[3] <= 2'd0;
-		end
+			num_password <= 0;
 		
-		else begin
-			/*assign password_input[num_inputs] to value in 'bits' 
-			need to confirm that this array indexing is correct*/
-			pw_in[num_inputs] <= bits;
-			num_inputs <= num_inputs + 1;
-			
-		
+			for (counter = 0; counter < 4 /* max size of the passwword*/; counter = counter + 1) begin
+				pw_sys[counter] <= 2'b00;
+				pw_in[counter] <= 2'b00;
+			end
 		end
 	end
 	
-	assign test = pw_in[0];
+	/*
+	always @(*) begin
+		
+		// if input_value
+		if (input_value)
+		begin
+			/assign password_input[num_inputs] to value in 'bits' 
+			//need to confirm that this array indexing is correct
+			pw_in[num_inputs] <= bits;
+			num_inputs <= num_inputs + 1;
+		end
+		
+		else if (store_value)
+		begin
+			//assign password[num_inputs] to value in 'bits' 
+			//need to confirm that this array indexing is correct
+			pw_sys[num_password] <= bits;
+			num_password <= num_password + 1;
+		end
+	end
+	*/
 	
-	//COMPARE CODE WHEN 'compare' SIGNAL SENT FROM CONTROLLER
-	//always @(compare) begin
-	//code for comparing the two registers goes here
-	//end
+	// comparing
+	
+	always @(posedge compare) begin
+		
+		for (index = 0; index < 4; index = index + 1) begin
+			if (pw_sys[index] == pw_in[index])
+			begin
+				correct_password <= 1'b1;
+				pw_in[index] <= 2'b00;
+			end
+		end
+		/*
+		if (correct_password == 1'b0)
+			incorrect_password <= 1'b1;
+		// reset the entire system
+		num_inputs <= 0;
+		*/
+	end
+	
 	
 endmodule
