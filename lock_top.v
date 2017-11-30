3e8a9c47de015617b0070c71dac6537e171158be
apostonaut <apostonaut@gmail.com>
2017-11-29 23:42:13 -0500

    instantiated code_checker and controller, and connnected wires

--- lock_top.v
+++ lock_top.v
@@ -11,38 +11,28 @@ module lock_top (LEDR, SW, HEX1, KEY);
 
 
 //instantiate modules
-controller_working controller	(	.storeButton(~KEY[3]), 
+controller_working controller	(	
+				.storeButton(~KEY[3]), 
 				.inputButton(~KEY[2]), 
 				.submitButton(~KEY[1]), 
 				.system_reset(~KEY[0]),
-			 .clk(CLOCK_50),
-			 .correct_password(correct_pw),
-			 .invalid_password(invalid_pw),
-			 .end_sleep(),
+			 	.clk(CLOCK_50),
+			 	.correct_password(correct_pw),
+			 	.invalid_password(invalid_pw),
+				 .end_sleep(end_sleep),
+				 
 			 /*OUTPUTS*/
-			 .input_value(),
-			 .store_value(),
-			 .compare(),
-			 .unlock(),
-			 .sleep()
+				 .input_value(input_value),
+				 .store_value(store_value),
+				 .compare(compare),
+				 .unlock(unlock),
+				 .sleep(sleep)
 
 			 //.pass_len(2'd3), NEED TO ADD THIS FEATURE 
-				);
+					);
 	
-/*
-module fsm_test
-(
-	input storeButton, inputButton, submitButton, system_reset, clk, 
-			correct_password, invalid_password, end_sleep,
-			
-	output reg input_value, store_value, compare, unlock, sleep
-);
-*/
+code_checker c0( 
 	
-	/*
-	wire compare, store_value, input_value, correct_pw, invalid_pw,
-	sleep, end_sleep, input_reset, system_reset;*/
-module code_checker( 
 	.input_value(input_value), //signal from Controller, store value in input register
 	.store_value(store_value), //signal from Controller, store value in system register
 	.compare(compare), 	// activates compare operation on posedge compare
@@ -60,10 +50,6 @@ module code_checker(
 		.correct_password(correct_pw), 
 		.incorrect_password(invalid_pw) //result of compare operation
 		);
-					
-code_checker c0();
-
-setup_panel s0();
 
 endmodule
 
