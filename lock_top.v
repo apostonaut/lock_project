c1ce36b8e10261559511f26492c186b13e9c79e9
apostonaut <apostonaut@gmail.com>
2017-11-29 23:37:32 -0500

    Update lock_top.v

--- lock_top.v
+++ lock_top.v
@@ -6,7 +6,7 @@ module lock_top (LEDR, SW, HEX1, KEY);
 
 //wires for connections between modules go here:
 	wire compare, store_value, input_value, correct_pw, invalid_pw,
-	sleep, end_sleep;
+	sleep, end_sleep, input_reset, system_reset;
 	
 
 
@@ -16,8 +16,8 @@ controller_working controller	(	.storeButton(~KEY[3]),
 				.submitButton(~KEY[1]), 
 				.system_reset(~KEY[0]),
 			 .clk(CLOCK_50),
-			 .correct_password(),
-			 .invalid_password(),
+			 .correct_password(correct_pw),
+			 .invalid_password(invalid_pw),
 			 .end_sleep(),
 			 /*OUTPUTS*/
 			 .input_value(),
@@ -38,13 +38,17 @@ module fsm_test
 	output reg input_value, store_value, compare, unlock, sleep
 );
 */
+	
+	/*
+	wire compare, store_value, input_value, correct_pw, invalid_pw,
+	sleep, end_sleep, input_reset, system_reset;*/
 module code_checker( 
-	.input_value(), //signal from Controller, store value in input register
-	.store_value(), //signal from Controller, store value in system register
-	.compare(), 	// activates compare operation on posedge compare
-	.input_reset(), //high when 
-		.system_reset(), //two different reset signals from Controller
-		.bits(), 
+	.input_value(input_value), //signal from Controller, store value in input register
+	.store_value(store_value), //signal from Controller, store value in system register
+	.compare(compare), 	// activates compare operation on posedge compare
+	.input_reset(input_reset), //high when 
+	.system_reset(system_reset), //two different reset signals from Controller
+	.bits(SW[1:0]), 
 		/*.in_test0(), 
 		.in_test1(), 
 		.in_test2(), 
@@ -53,8 +57,8 @@ module code_checker(
 		.sys_test1(), 
 		.sys_test2(), 
 		.sys_test3(),*/
-		.correct_password(), 
-		.incorrect_password() //result of compare operation
+		.correct_password(correct_pw), 
+		.incorrect_password(invalid_pw) //result of compare operation
 		);
 					
 code_checker c0();
